406e309a73338ab4320f42860af1079726ba47c8
Imanuel Costigan <i.costigan@me.com>
2014-06-15 08:17:52 +1000

    Address CRAN comments
    - Changed package name to `arnie` from `arnieattheboxoffice` to address
    Ripley comments
    - Changed license to CC 3.0 from CC 4.0 to address Ripley comments
    - Added example to address Ripley comments

--- R/arnie.R
+++ R/arnie.R
@@ -1,4 +1,4 @@
-#' Arnie at the Box Office
+#' Arnie at the box office
 #'
 #' The data set contains the weekend box office performance of Arnold
 #' Schwarzenegger's movies from 1982 to the present. The data was scraped
@@ -7,6 +7,9 @@
 #' @format A CSV file with nine fields: \code{Name}, \code{Date}, \code{Rank},
 #' \code{Gross}, \code{Percentage.Change}, \code{Theaters}, \code{Per.Theater},
 #' \code{Total.Gross} and \code{Days}.
+#' @examples
+#' data(arnie)
+#' head(arnie)
 #' @references
 #' \href{http://www.the-numbers.com/new/person/128750401-Arnold-Schwarzenegger}{The Numbers: Arnold Schwarzenegger}
 "arnie"
